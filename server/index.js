b9d07ef693092b0766dc1613b84dc60a61514905
ezexgonzalez <ezexgonzalez@outlook.com>
2025-12-01 02:34:47 -0300

    backend deploy config

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ import taskRoutes from "./routes/task.routes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
