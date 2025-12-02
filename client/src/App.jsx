e5a575fe1f7bb0ed310f50056420e0f8dfcbe751
ezexgonzalez <ezexgonzalez@outlook.com>
2025-12-02 03:09:32 -0300

    add wakeup api render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
 import AppRouter from "./router/AppRouter";
+import { useEffect } from "react";
+
 function App() {
-  return (
-    <AppRouter/>
-  );
+  useEffect(() => {
+    const wakeUp = async () => {
+      try {
+        // VITE_API_URL = https://.../api
+        const baseUrl = import.meta.env.VITE_API_URL.replace(/\/api\/?$/, "");
+        await fetch(`${baseUrl}/`);
+      } catch (e) {
+        console.log("Wakeup ping failed", e);
+      }
+    };
+
+    wakeUp();
+  }, []);
+  return <AppRouter />;
 }
 
 export default App;
